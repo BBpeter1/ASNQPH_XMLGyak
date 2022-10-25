4f0d0f60d5954799483a7e08e98c8280f406d99d
BBpeter1 <79256968+BBpeter1@users.noreply.github.com>
2022-10-25 10:45:13 +0200

    7. heti gyakorlati feladatok feltöltése

---
+++ ASNQPH_1019/Xsd_ASNQPH.java
@@ -0,0 +1,37 @@
+package Sax_ASNQPH_1019;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.SAXException;
+
+
+public class Xsd_ASNQPH {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Validation succesful? "+validateXMLSchema("BBP_kurzusfelvetel.xsd", "BBP_kurzusfelvetel.xml"));
+
+	} //end main
+	
+	public static boolean validateXMLSchema(String xsdPath, String xmlPath){
+        
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File("BBP_kurzusfelvetel.xsd"));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File("BBP_kurzusfelvetel.xml")));
+        } catch (IOException | SAXException e) {
+            System.out.println("Exception: "+e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+} //end class
